f9cdcdf5ed5b0a011872439d7833db343c95abb1
princebhanderi <bhanderiprince03@gmail.com>
2026-02-04 01:17:05 +0530

    [Constant] Added more constants

---
+++ lib/utils/constants/sizes.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class JJSizes {
+  //Padding and Margin sizes
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+  static const double xxl = 40.0;
+
+  //Icon sizes
+  static const double iconXs = 12.0;
+  static const double iconSm = 16.0;
+  static const double iconMd = 24.0;
+  static const double iconLg = 32.0;
+
+  //Font sizes
+  static const double fontXs = 10.0;
+  static const double fontSm = 12.0;
+  static const double fontMd = 14.0;
+  static const double fontLg = 18.0;
+  static const double fontXl = 24.0;
+
+  //Button sizes with height,width,radius
+  static const double buttonHeight = 18.0;
+  static const double buttonWidth = 32.0;
+  static const double buttonRadius = 8.0;
+  static const double buttonWidthFull = double.infinity;
+
+  //default spacing between sections
+  static const double defaultSpace = 24.0;
+  static const double spaceBtwItems = 16.0;
+  static const double spaceBtwSections = 32.0;
+
+  
+}
